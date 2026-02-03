904b44bc5338fed96a32b3091e9470d2da33f3b0
The YackyYam <smithincanton@gmail.com>
2026-02-03 11:02:32 -0500

    Add files via upload

---
+++ fabtech_install.ps1
@@ -0,0 +1,30 @@
+$fab_app_scanner = "https://www.fabulatech.com/dists/scanrdp/scanner-for-remote-desktop-server-64bit.msi"
+$fab_file_scanner = "scanner-for-remote-desktop-server-64bit.msi"
+$fab_app_usb = "https://www.fabulatech.com/dists/usbrdp/usb-for-remote-desktop-server-64bit.msi"
+$fab_file_usb = "usb-for-remote-desktop-server-64bit.msi"
+$fab_app_webcam = "https://www.fabulatech.com/dists/camrdp/webcam-for-remote-desktop-server-64bit.msi"
+$fab_file_webcam = "webcam-for-remote-desktop-server-64bit.msi"
+$fab_app_sound = "https://www.fabulatech.com/dists/sndrdp/sound-for-remote-desktop-server-64bit.msi"
+$fab_file_sound = "sound-for-remote-desktop-server-64bit.msi"
+
+$ProgressPreference = 'SilentlyContinue'
+
+Write-host "Downloading: " $fab_file_scanner
+Invoke-WebRequest -Uri $fab_app_scanner -OutFile $fab_file_scanner
+Write-host "Installing."
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "`"$fab_file_scanner`"", "/qn", "/norestart" -Wait   
+
+Write-host "Downloading: " $fab_file_usb
+Invoke-WebRequest -Uri $fab_app_usb -OutFile $fab_file_usb
+Write-host "Installing."
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "`"$fab_file_usb`"", "/qn", "/norestart" -Wait   
+
+Write-host "Downloading: " $fab_file_webcam
+Invoke-WebRequest -Uri $fab_app_webcam -OutFile $fab_file_webcam
+Write-host "Installing."
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "`"$fab_file_webcam`"", "/qn", "/norestart" -Wait   
+
+Write-host "Downloading: " $fab_file_sound
+Invoke-WebRequest -Uri $fab_app_sound -OutFile $fab_file_sound
+Write-host "Installing."
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "`"$fab_file_sound`"", "/qn", "/norestart" -Wait   
---
+++ fabtech_uninstall.ps1
@@ -0,0 +1,20 @@
+Write-Host "Gathering installed programs. Please wait. Takes about 10 sec."
+
+$fab_installed = Get-WmiObject Win32_Product | Where-Object { $_.Vendor -like "FabulaTech" }
+
+Clear-Host
+
+foreach ($row in $fab_installed) {        
+if ($row -like "*scanner*") {
+Write-Host "Uninstalling version: " $row.Name
+Get-Package -Name $row.Name | Uninstall-Package -Force
+}elseif ($row -like "*usb*") {
+Write-Host "Uninstalling version: " $row.Name
+Get-Package -Name $row.Name | Uninstall-Package -Force
+}elseif ($row -like "*webcam*") {
+Write-Host "Uninstalling version: " $row.Name
+Get-Package -Name $row.Name | Uninstall-Package -Force
+}elseif ($row -like "*sound*") {
+Write-Host "Uninstalling version: " $row.Name
+Get-Package -Name $row.Name | Uninstall-Package -Force
+}
